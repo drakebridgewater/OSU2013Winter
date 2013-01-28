96512e2ab4a0ba7979c2bb0c1f9e1202b2d7393b
Drake Bridgewater <drake.bridgewater@gmail.com>
2013-01-28 10:44:19 -0800

    Not sure but saving

--- CS 311.HW 2/Archiver/main.c
+++ CS 311.HW 2/Archiver/main.c
@@ -1,119 +1,37 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <stdio.h>
-
-#define BLOCKSIZE 1
 
 int main(int argc, char **argv)
 {
-    char *input = argv[1];
-	char *output = argv[2];
+    if(argc != 2)
+        return 1;
 
-	int in_fd;
-	int out_fd;
+    struct stat fileStat;
+    if(stat(argv[1],&fileStat) < 0)
+        return 1;
 
-	char buf[BLOCKSIZE];
+    printf("Information for %s\n",argv[1]);
+    printf("---------------------------\n");
+    printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+    printf("Number of Links: \t%d\n",fileStat.st_nlink);
+    printf("File inode: \t\t%d\n",fileStat.st_ino);
 
-	int num_read;
-	int num_written;
+    printf("File Permissions: \t");
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n\n");
 
-	char *theFilename = input;
-	char *fileModTimestamp;
-	char *fileOwnerID;
-	char *fileGroupID;
-	char *fileMode;
-	int  *fileSize;
-	char *fileMagic;
+    printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
 
-    struct stat sb;
-
-   if (argc != 2) {
-        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-   if (stat(argv[1], &sb) == -1) {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
-
-   printf("File type:                ");
-
-   switch (sb.st_mode & S_IFMT) {
-    case S_IFBLK:  printf("block device\n");            break;
-    case S_IFCHR:  printf("character device\n");        break;
-    case S_IFDIR:  printf("directory\n");               break;
-    case S_IFIFO:  printf("FIFO/pipe\n");               break;
-    case S_IFLNK:  printf("symlink\n");                 break;
-    case S_IFREG:  printf("regular file\n");            break;
-    case S_IFSOCK: printf("socket\n");                  break;
-    default:       printf("unknown?\n");                break;
-    }
-
-   printf("I-node number:            %ld\n", (long) sb.st_ino);
-
-   printf("Mode:                     %lo (octal)\n",
-            (unsigned long) sb.st_mode);
-
-   printf("Link count:               %ld\n", (long) sb.st_nlink);
-    printf("Ownership:                UID=%ld   GID=%ld\n",
-            (long) sb.st_uid, (long) sb.st_gid);
-
-   printf("Preferred I/O block size: %ld bytes\n",
-            (long) sb.st_blksize);
-    printf("File size:                %lld bytes\n",
-            (long long) sb.st_size);
-    printf("Blocks allocated:         %lld\n",
-            (long long) sb.st_blocks);
-
-   printf("Last status change:       %s", ctime(&sb.st_ctime));
-    printf("Last file access:         %s", ctime(&sb.st_atime));
-    printf("Last file modification:   %s", ctime(&sb.st_mtime));
-
-   exit(EXIT_SUCCESS);
-
-
-
-
-	off_t file_size;
-	off_t copied;
-
-	in_fd = open(input, O_RDONLY);
-	if(in_fd == -1){
-		perror("Can't open input file");
-		exit(-1);
-	}
-
-	out_fd = open(output, O_WRONLY | O_CREAT, 0777);
-	if(out_fd == -1){
-		perror("Can't open output file");
-		exit(-1);
-	}
-
-	file_size = lseek(in_fd, 0, SEEK_END);
-	copied = 0;
-	lseek(in_fd, -1, SEEK_END);
-
-	while(copied < file_size){
-		num_read = read(in_fd, buf, BLOCKSIZE);
-		num_written = write(out_fd, buf, BLOCKSIZE);
-
-		if (num_read != num_written){
-			perror("Error writing file");
-			unlink(output);
-			exit(-1);
-		}
-
-		copied += num_written;
-
-		lseek(in_fd, -2, SEEK_CUR);
-	}
-
-	return 0;
+    return 0;
 }
